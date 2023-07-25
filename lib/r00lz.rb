22215a6ec259b8bd3e8b0abd8c6e463ed975aac1
Chris Tulin <christulin@gmail.com>
2023-07-25 15:27:40 -0700

    update new App class

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -2,5 +2,10 @@ require "r00lz/version"
 
 module R00lz
   class Error < StandardError; end
-  # Your code goes here...
+  class App
+    def call(env)
+      [200, {'Content-Type' => 'text/html'},
+        ["Hello from r00lz!"]]
+    end
+  end
 end
